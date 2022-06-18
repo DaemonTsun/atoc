78e9781c46fd5b6b0f4e9b1efdde26d8f908c495
DaemonTsun <96687758+DaemonTsun@users.noreply.github.com>
2022-06-18 09:23:40 -0400

    Initial commit

---
+++ src/config.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#define ATOC_NAME "atoc"
+#define ATOC_AUTHOR "DaemonTsun"
+#define ATOC_VERSION "0.0.1"
---
+++ src/main.cpp
@@ -0,0 +1,137 @@
+
+#include <stdexcept>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <string.hpp>
+#include <psp_elf.hpp>
+
+#include "config.hpp"
+
+struct arguments
+{
+    std::string output_file; // -o, --output
+    std::string log_file;    // --log
+    bool verbose;            // -v, --verbose
+
+    std::string input_file;
+};
+
+const arguments default_arguments{
+    .output_file = "",
+    .log_file = "",
+    .verbose = false,
+    .input_file = ""
+};
+
+void print_usage()
+{
+    puts("Usage: " ATOC_NAME " [-h] [-g] [-o OUTPUT] [-p] [-a VADDR] [-v] OBJFILE\n"
+         "\n"
+         ATOC_NAME " v" ATOC_VERSION ": allegrex to c decompiler\n"
+         "by " ATOC_AUTHOR "\n"
+         "\n"
+         "Optional arguments:\n"
+         "  -h, --help                  show this help and exit\n"
+         "  -o OUTPUT, --output OUTPUT  output filename (default: stdout)\n"
+         "  --log LOGFILE               output all information messages to LOGFILE (stdout by default)\n"
+         "  -v, --verbose               verbose progress output\n"
+         "\n"
+         "Arguments:\n"
+         "  OBJFILE      ELF object file to decompile\n"
+         );
+}
+
+void parse_arguments(int argc, const char **argv, arguments *out)
+{
+    for (int i = 1; i < argc;)
+    {
+        std::string arg = argv[i];
+
+        if (arg == "-h" || arg == "--help")
+        {
+            print_usage();
+            exit(EXIT_SUCCESS);
+        }
+
+        if (arg == "-o" || arg == "--output")
+        {
+            if (i >= argc - 1)
+                throw std::runtime_error(str(arg, " expects a positional argument: the output file"));
+
+            out->output_file = argv[i+1];
+            i += 2;
+            continue;
+        }
+
+        if (arg == "--log")
+        {
+            if (i >= argc - 1)
+                throw std::runtime_error(str(arg, " expects a positional argument: the log file"));
+
+            out->log_file = argv[i+1];
+            i += 2;
+            continue;
+        }
+
+        if (arg == "-v" || arg == "--verbose")
+        {
+            out->verbose = true;
+            ++i;
+            continue;
+        }
+
+        // etc
+        if (begins_with(arg, str("-")))
+            throw std::runtime_error(str("unknown argument '", arg, "'"));
+
+        if (out->input_file.empty())
+        {
+            out->input_file = arg;
+            ++i;
+            continue;
+        }
+        else
+            throw std::runtime_error(str("unexpected argument '", arg, "'"));
+    }
+}
+
+int main(int argc, const char **argv)
+try
+{
+    arguments args = default_arguments;
+
+    parse_arguments(argc, argv, &args);
+
+    if (args.input_file.empty())
+        throw std::runtime_error("expected input file");
+
+    // get logfile
+    FILE *logfd = stdout;
+
+    if (!args.log_file.empty())
+        logfd = fopen(args.log_file.c_str(), "a");
+
+    file_stream log(logfd);
+
+    if (!log)
+        throw std::runtime_error("could not open file to log");
+
+    file_stream in(args.input_file, "rb");
+
+    if (!in)
+        throw std::runtime_error("could not open input file");
+
+    in.calculate_size();
+    
+    // do_the_thing(&in, &log, args);
+
+    in.close();
+    return 0;
+}
+catch (std::runtime_error &e)
+{
+    fprintf(stderr, "error: %s\n", e.what());
+    return 1;
+}
